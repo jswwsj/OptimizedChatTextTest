534c9042213cccdf014031ac97ba1c30c328d016
jiangsiwei <siweijiang@sohu-inc.com>
2025-12-03 17:42:58 +0800

    Initial Commit

---
+++ OptimizedChatTextTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OptimizedChatTextTest
+//
+//  Created by jiangsiwei on 2025/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
